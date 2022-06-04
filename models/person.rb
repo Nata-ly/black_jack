cabf291843941a49574d14e86facc2bbee152d21
Natalia <nikifor1114@mail.ru>
2022-06-04 21:47:23 +0700

    Add method calculate sum points

--- models/person.rb
+++ models/person.rb
@@ -10,5 +10,23 @@ class Person
 
   def add_card(deck)
     @cards << deck.draw_card
+    calculate_sum_points
+  end
+
+  def calculate_sum_points
+    @sum = 0
+    ace = false
+    @cards.each do |card|
+      @sum += points(card_nominal = card.split(' ').first)
+      ace = true if card_nominal == 'T'
+    end
+    @sum += 10 if ace == true && @sum <= 11
+    @sum
+  end
+
+  def points(card)
+    return 10 if ['K', 'D', 'V'].include?(card)
+    return 1 if card == 'T'
+    card.to_i
   end
 end
